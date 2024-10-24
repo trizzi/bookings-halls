2e95fda5d48eea849f1589026973847e1dab0187
trizzi <patrickakhamiogu@gmail.com>
2024-10-24 23:40:15 +0100

    Show/hide nav links

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,28 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
 import Logo from '@/assets/images/logo.svg';
 import { FaUser, FaSignOutAlt, FaSignInAlt, FaBuilding } from 'react-icons/fa';
 import destroySession from '@/app/actions/destroySession';
+import checkAuth from '@/app/actions/checkAuth';
 import { toast } from 'react-toastify';
 
 const Header = () => {
 	const router = useRouter();
+	const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+	useEffect(() => {
+		const fetchAuthStatus = async () => {
+			const result = await checkAuth();
+			setIsAuthenticated(result.isAuthenticated);
+		};
+
+		fetchAuthStatus();
+	}, []);
+
 	const handleLogout = async () => {
 		const { success, error } = destroySession();
 		if (success) {
@@ -40,16 +53,20 @@ const Header = () => {
 									Rooms
 								</Link>
 								{/* <!-- Logged In Only --> */}
-								<Link
-									href='/bookings'
-									className='rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white'>
-									Bookings
-								</Link>
-								<Link
-									href='/rooms/add'
-									className='rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white'>
-									Add Room
-								</Link>
+								{isAuthenticated && (
+									<>
+										<Link
+											href='/bookings'
+											className='rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white'>
+											Bookings
+										</Link>
+										<Link
+											href='/rooms/add'
+											className='rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white'>
+											Add Room
+										</Link>
+									</>
+								)}
 							</div>
 						</div>
 					</div>
@@ -57,24 +74,32 @@ const Header = () => {
 					<div className='ml-auto'>
 						<div className='ml-4 flex items-center md:ml-6'>
 							{/* <!-- Logged Out Only --> */}
-							<Link
-								href='/login'
-								className='mr-3 text-gray-800 hover:text-gray-600'>
-								<FaSignInAlt className='inline mr-1' /> Login
-							</Link>
-							<Link
-								href='/register'
-								className='mr-3 text-gray-800 hover:text-gray-600'>
-								<FaUser className='inline mr-1'></FaUser> Register
-							</Link>
-							<Link href='/rooms/my'>
-								<FaBuilding className='inline mr-1' /> My Rooms
-							</Link>
-							<button
-								onClick={handleLogout}
-								className='mx-3 text-gray-800 hover:text-gray-600'>
-								<FaSignOutAlt className='inline mr-1' /> Sign Out
-							</button>
+							{!isAuthenticated && (
+								<>
+									<Link
+										href='/login'
+										className='mr-3 text-gray-800 hover:text-gray-600'>
+										<FaSignInAlt className='inline mr-1' /> Login
+									</Link>
+									<Link
+										href='/register'
+										className='mr-3 text-gray-800 hover:text-gray-600'>
+										<FaUser className='inline mr-1'></FaUser> Register
+									</Link>
+								</>
+							)}
+							{isAuthenticated && (
+								<>
+									<Link href='/rooms/my'>
+										<FaBuilding className='inline mr-1' /> My Rooms
+									</Link>
+									<button
+										onClick={handleLogout}
+										className='mx-3 text-gray-800 hover:text-gray-600'>
+										<FaSignOutAlt className='inline mr-1' /> Sign Out
+									</button>
+								</>
+							)}
 						</div>
 					</div>
 				</div>
@@ -83,17 +108,21 @@ const Header = () => {
 			{/* <!-- Mobile menu --> */}
 			<div className='md:hidden'>
 				<div className='space-y-1 px-2 pb-3 pt-2 sm:px-3'>
-					<Link
-						href='/'
-						className='block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-gray-700 hover:text-white'>
-						Rooms
-					</Link>
-					{/* <!-- Logged In Only --> */}
-					<Link
-						href='/bookings'
-						className='block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-gray-700 hover:text-white'>
-						Bookings
-					</Link>
+					{isAuthenticated && (
+						<>
+							<Link
+								href='/'
+								className='block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-gray-700 hover:text-white'>
+								Rooms
+							</Link>
+							{/* <!-- Logged In Only --> */}
+							<Link
+								href='/bookings'
+								className='block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-gray-700 hover:text-white'>
+								Bookings
+							</Link>
+						</>
+					)}
 					<Link
 						href='/rooms/add'
 						className='block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-gray-700 hover:text-white'>
